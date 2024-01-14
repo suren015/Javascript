277de9fa7f4e5089c9101d3d5a18afb883f075ee
Surendra Tholiya <114131620+suren015@users.noreply.github.com>
2024-01-14 05:32:34 +0000

    save

--- Js-Basic/03_conversionOper.js
+++ Js-Basic/03_conversionOper.js
@@ -54,10 +54,33 @@ let str2 = "Tholiya"
 
 console.log(str1+str2);
 
-console.log("1"+2);
-console.log(1+"2")
+// Note => Don't do these type of code ::
 
+console.log("1"+2);    // String first then all other treat as string 
 console.log("1"+2+2)
 
+console.log(1+"2")    // String last then all first operation and then string
 console.log(1+2+"2")
+
+console.log((3+4)*5%3);
+
+// Special Conversion =>
+
+console.log(true);
+console.log(+true); // return 1
+// console.log(true+);  Gives an error
+console.log(+""); // return 0
+
+
+let gc = 100;
+// gc++;
+console.log(gc);
+console.log(++gc);
+
+gc = 100;
+console.log(gc);
+console.log(gc++);
+console.log(gc);
+
+// For above process, u can use this link :: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 
