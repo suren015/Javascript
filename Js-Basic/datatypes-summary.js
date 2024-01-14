13fba6fa5981a3944a9e5205f2ab592b412421f3
Surendra Tholiya <114131620+suren015@users.noreply.github.com>
2024-01-14 07:26:21 +0000

    save

--- Js-Basic/datatypes-summary.js
+++ Js-Basic/datatypes-summary.js
@@ -54,3 +54,36 @@ String	           "string"	                   String
 Symbol	           "symbol"	                   Symbol
      
 */
+
+
+
+
+/* ----------------------------------------------------------------------- */
+
+// Stack in Primitive , Heapin Non-Primitive Datatype
+
+
+
+let myName = "Suren";
+
+let anothername = myName;
+anothername = "tholiya"; 
+
+console.log(myName);
+console.log(anothername);
+
+
+
+let user1 = {
+    email: "user1@gmail.com",
+    upi: "user@ybl"
+}
+
+
+// Non-Primitive Dat-type goes by reference ::
+
+let user2 = user1;
+user2.email = "user2.gmail.com";
+
+console.log(user1.email);
+console.log(user2.email);
