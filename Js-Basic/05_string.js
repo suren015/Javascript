80fb1247d93617514ca749189fb57711f6c6eb8b
Surendra Tholiya <114131620+suren015@users.noreply.github.com>
2024-01-15 06:02:35 +0000

    save

--- Js-Basic/05_string.js
+++ Js-Basic/05_string.js
@@ -25,6 +25,8 @@ console.log(getname.substring(2, 5));   // print substring from start idx to end
 
 console.log(getname.indexOf('end'));  // return the position of first character of the substring
 
+// The slice() method of String values extracts a section of this string and returns it as a new string, without modifying the original string.
+
 let another = getname.slice(-4,6);    // For Negative values that is checking from the backside
 console.log(another);    
 
@@ -35,5 +37,18 @@ console.log(newStringOne.trim());       //print whole string without space :: Re
 
 const url = "https://suren.com/suren%20tholiya";
 
-console.log(url.replace('%20','-'));
-console.log(url.includes("suren"));
+console.log(url.replace('%20','-'));  // Replace '%20' with '-'
+console.log(url.includes("suren"));   // is 'suren' is present in url ?
+
+ 
+
+// The split() method of String values takes a pattern and divides this string into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.
+
+// Syntax :: split(separator, limit)
+// Syntax :: split(separator)
+
+const str = 'The quick brown fox jumps over the lazy dog';
+
+const words = str.split(' ');    // String converted into array based on ' '
+console.log(words[3]);   // return fox
+console.log(words[8]);   // return dog
