3d8746e80e7c5ed1fa1bb08dc5988af33b2f5384
Surendra Tholiya <114131620+suren015@users.noreply.github.com>
2024-01-19 10:49:19 +0000

    save

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// "if" Statement ::>
+
+const isUserLoggedIn = true;
+if(isUserLoggedIn){
+    console.log(`Yes, user logged in.`);
+}
+
+// Operators:: <, >, <=, >=, ==, !=, ===
+if(2 == "2"){                           // Only for value checking
+    console.log("Executed-1");
+}
+if(2 === "2"){
+    console.log("Wxwctued-2");         // Both value and data type checking
+}
+
+const temp = 41;
+if(temp === 41){
+    console.log("temp is equal to 41");
+}
+
+const score = 100;
+if(score>50){
+    let power = 200;
+    console.log(`power is: ${power}`);
+}
+// console.log(`Outside power is: ${power}`);   // This line gives an error, if we use var instead of let then it execute as var is globally declared variable.
