6253ca9defd4f14c6785df44783f0b46836f6341
Surendra Tholiya <114131620+suren015@users.noreply.github.com>
2024-01-25 14:29:58 +0000

    save

---
+++ 07_project/5-keyboard/index.js
@@ -0,0 +1,24 @@
+// Selecting insert id
+const insert = document.getElementById('insert');
+
+// add event listener on window
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+    <div class='color'>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Keycode</th> 
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td> 
+      <td>${e.code}</td>
+    </tr>
+    
+  </table>
+    </div>
+  `;
+});
+
